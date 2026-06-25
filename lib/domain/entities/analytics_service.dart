f52599aa3f4e3980337d1f24b9250ce0aad8c7b3
ferry-wibisono <kodenatan17@gmail.com>
2026-06-25 14:12:04 +0700

    unused docs

--- lib/domain/entities/analytics_service.dart
+++
@@ -1,6 +0,0 @@
-abstract interface class AnalyticsService {
-  Future<void> logScreenView({required String screenName, String? screenClass});
-  Future<void> logEvent(String name, {Map<String, Object>? parameters});
-  Future<void> setUserId(String? id);
-  Future<void> setUserProperty({required String name, required String? value});
-}
